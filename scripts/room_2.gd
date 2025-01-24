b121e5e3b80e484f6616e07219e93eaba713a29f
Abloodor <zlargclement@gmail.com>
2025-01-24 17:09:50 +0100

    commit

---
+++ scripts/room_2.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var notes_ramasses : Array = []
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_note_catch(note : Area2D) -> void:
+	var healthBar = get_node("CharacterBody2D/Camera2D/TextureProgressBar")
+	notes_ramasses.append(note)
+	healthBar.soigner(100)
+
+
+func _on_texture_progress_bar_zéro() -> void:
+	get_tree().change_scene_to_file("res://scenes/endMenu.tscn")
