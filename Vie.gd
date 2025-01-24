a60e0a89a1133c9b412a60a93fc70dc93544a74f
Abloodor <zlargclement@gmail.com>
2025-01-24 09:20:58 +0100

    barreDeVie

---
+++ Vie.gd
@@ -0,0 +1,20 @@
+extends TextureProgressBar
+
+var vitesse = 1
+signal zéro
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	value -= 1*vitesse
+	if value==0:
+		emit_signal("zéro")
+
+func updateVitesse():
+	vitesse+=1
+
+func add(soin):
+	value+=soin
