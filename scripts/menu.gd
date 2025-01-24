0f11a3060d47b776283e873067f60a240dadc56a
Clément Lerouley <100298184+clemleri@users.noreply.github.com>
2025-01-24 13:37:47 +0100

    ajout changement de scène

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/Room1.tscn")
 
 func _on_tutorial_pressed() -> void:
 	pass # Replace with function body.
