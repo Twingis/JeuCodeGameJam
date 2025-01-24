a2fc443dc7c31642d8285f97b27450a5d95a334f
Twingis <loris.drouhot@gmail.com>
2025-01-24 15:19:37 +0100

    TOUT MARCHE SI CA MARCHE C VOUS

--- scripts/game.gd
+++ scripts/game.gd
@@ -7,9 +7,11 @@ extends Node2D
 func _process(delta: float) -> void:
 	pass
 
+
 func _on_note_catch() -> void:
 	var healthBar = get_node("CharacterBody2D/Camera2D/TextureProgressBar")
-	healthBar.add(100)
+	healthBar.soigner(100)
+
 
 func _on_texture_progress_bar_zéro() -> void:
 	get_tree().change_scene_to_file("res://scenes/endMenu.tscn")
