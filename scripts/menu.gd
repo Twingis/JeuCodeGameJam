7302c517042e727b9d784b1a581b2dd1d7c47197
Abloodor <zlargclement@gmail.com>
2025-01-24 17:04:36 +0100

    tuto

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -4,7 +4,7 @@ func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Room1.tscn")
 
 func _on_tutorial_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/tutoriel.tscn")
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/optionsMenu.tscn")
