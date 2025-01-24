c16b9ea0f49c51f981444a9a5750b7da98d0eb93
Twingis <loris.drouhot@gmail.com>
2025-01-24 15:21:09 +0100

    Revert "Merge branch 'main' of https://github.com/Twingis/JeuCodeGameJam"
    This reverts commit fb2bc42e814d60aae00e8e81c755f02dcb681ce3, reversing
    changes made to a2fc443dc7c31642d8285f97b27450a5d95a334f.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 120.0
-const JUMP_VELOCITY = -260.0
+const JUMP_VELOCITY = -250.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
