d1a1903be45a8ec38dd3762b61f6be3f711d9778
Clément Lerouley <100298184+clemleri@users.noreply.github.com>
2025-01-24 19:02:22 +0100

    correction end menu

--- scripts/end_menu.gd
+++ scripts/end_menu.gd
@@ -2,7 +2,9 @@ extends Control
 
 
 func _on_replay_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Room1.tscn")
+	var rand_room_num = randi_range(1,2)
+	var path = "res://scenes/levels/random_level_"+str(rand_room_num)+".tscn"
+	get_tree().change_scene_to_file(path)
 
 
 func _on_back_pressed() -> void:
