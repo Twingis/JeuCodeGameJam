2f8316e37d01400f49cdf2f9b314af9492f8c638
Benjamin Lecomte <80157210+Benjam1Lct@users.noreply.github.com>
2025-01-25 06:20:13 +0100

    update

--- scripts/vue_d&d.gd
+++ scripts/vue_d&d.gd
@@ -122,8 +122,6 @@ func _on_check_pressed() -> void:
 		var rand_room_num = randi_range(1,6)
 		var path = "res://scenes/levels/random_level_"+str(rand_room_num)+".tscn"
 		get_tree().change_scene_to_file(path)
-		var next_scene = list_scenes[randi()%list_scenes.size()-1]
-		get_tree().change_scene_to_file(next_scene)
 	else:
 		_on_reset_notes_pressed()
 
