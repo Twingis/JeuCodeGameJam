62947dccd903cdc158d7f6f92c1495c7528bb776
Clément Lerouley <100298184+clemleri@users.noreply.github.com>
2025-01-25 05:20:49 +0100

    udpate_cutescene

--- scripts/cutscene.gd
+++ scripts/cutscene.gd
@@ -6,3 +6,7 @@ func _ready() -> void:
 	
 func _on_video_stream_player_finished() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/random_level_1.tscn")
+
+
+func _on_skip_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/random_level_1.tscn")
