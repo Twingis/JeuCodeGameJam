c5473435601685b64ad8fc0c2e6dc4e1fa98c009
Clément Lerouley <100298184+clemleri@users.noreply.github.com>
2025-01-25 05:13:36 +0100

    update_cutscene_with_voice

--- scripts/cutscene.gd
+++ scripts/cutscene.gd
@@ -1,7 +1,8 @@
 extends Control
 
-
-
-
+func _ready() -> void:
+	$Bruitage.play()
+	$Narration.play()
+	
 func _on_video_stream_player_finished() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/random_level_1.tscn")
