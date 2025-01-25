89d9be7308044e05b2f3a6ddbac766261328c4f1
Macfeunec <40341052+McFennec@users.noreply.github.com>
2025-01-25 06:18:29 +0100

    les labels et tout

--- scripts/room1.gd
+++ scripts/room1.gd
@@ -6,7 +6,10 @@ var notes_sorted = []
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(notes_ramasses)
+	if notes_ramasses.size() < melody.size() :
+		$Label.text = "Tu n'as pas aidé toutes les notes :("
+	else :
+		$Label.text = "Dépèche toi de sortir !"
 
 func _ready() -> void:
 	notes_ramasses = []
@@ -26,10 +29,7 @@ func _on_texture_progress_bar_zéro() -> void:
 	get_tree().change_scene_to_file("res://scenes/endMenu.tscn")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if notes_ramasses.size() < melody.size() :
-		$Label.text = "Vous n'avez pas toutes les notes"
-	else :
-		$Label.text = ""
+	if notes_ramasses.size() >= melody.size() :
 		print(notes_ramasses)
 		for note in notes_ramasses:
 			Global.update_list_notes(note.id)
