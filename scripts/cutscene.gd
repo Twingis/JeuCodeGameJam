3f399676053a1c64fb5b7251473b198175de147f
Benjamin Lecomte <80157210+Benjam1Lct@users.noreply.github.com>
2025-01-25 07:35:49 +0100

    add_solution_button

--- scripts/cutscene.gd
+++ scripts/cutscene.gd
@@ -11,6 +11,6 @@ func _on_skip_pressed() -> void:
 	go_to_game()
 
 func go_to_game() :
-	var rand_room_num = randi_range(1,6)
+	var rand_room_num = randi_range(1,5)
 	var path = "res://scenes/levels/random_level_"+str(rand_room_num)+".tscn"
 	get_tree().change_scene_to_file(path)
